da36d03577071c1bb90ae222987f7e6ffffbe419
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2023-03-12 23:55:31 +0600

    todo endpoint

--- app.js
+++ app.js
@@ -2,16 +2,38 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require("dotenv");
+const todos = require("./data/todo")
 dotenv.config();
 var cors = require('cors')
 app.use(cors());
 
 
+// Connect to Database
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.62sfwlg.mongodb.net/?retryWrites=true&w=majority`;
 
+mongoose.connect(url,{
+})
+.then(()=> console.log('Database connection established'))
+.catch(err => console.log(err));
+
+
+
+// Default Get Route
 app.get('/', (req, res) => {
-  res.send('Hello, From Notebook Server! ✌')
+  res.send('Hello, From Notebook Server! ✌✔')
 });
 
+// Get All Route
+app.get('/api/todo',(req,res)=>{
+  res.json(todos);
+})
+
+// Get Single Todo
+app.get('/api/todo/:id', (req,res)=>{
+  const todo = todos.find((n)=> n._id === req.params.id);
+  res.send(todo)
+  console.log(todo);
+})
 
 
 module.exports = app;
---
+++ routes/todoRoutes.js
@@ -0,0 +1,19 @@
+// const express = require('express');
+// const router = express.Router();
+// const { getNotes, createNote,getNoteById,updateNote,DeleteNote} = require('../controllers/noteController');
+// const verifyJWT = require('../middlewares/verifyJWT');
+
+
+// // commit to server Restart
+
+// router.route('/').get(verifyJWT,getNotes);
+// router.route('/create').post(verifyJWT,createNote);
+// router.route('/:id').get(getNoteById)
+//       .put(verifyJWT,updateNote)
+//       .delete(verifyJWT,DeleteNote)
+ 
+
+
+
+
+// module.exports = router;
